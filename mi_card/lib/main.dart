d4aa31dd4d50186acf0c1eeeaeea1e4b39584479
Aximilli1212 <aximill1212@gmail.com>
2020-03-13 13:17:22 +0000

    mainAxisAlignment.end for all my boxes

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           ),
           body:SafeArea(
             child: Column(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 Container(
                   height:100.0,
