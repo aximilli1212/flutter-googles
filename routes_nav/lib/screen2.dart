49664fbe31261673b3e718387b79ff69e9a25476
Aximilli1212 <aximill1212@gmail.com>
2020-03-20 17:30:38 +0000

    add screen 2 for nav

---
+++ routes_nav/lib/screen2.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white10,
+      appBar: AppBar(
+        title: Text('Screen0'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: RaisedButton(
+              color: Colors.purple,
+              onPressed: (){},
+              child: Text('Goto Screen 1', style: TextStyle(
+                color: Colors.white,
+              ),),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
