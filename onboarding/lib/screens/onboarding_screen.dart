b5bb00fd7ff7a8a3e5589f775623a41e568de910
Aximilli1212 <aximill1212@gmail.com>
2020-03-21 13:52:54 +0000

    Add custom default image

--- onboarding/lib/screens/onboarding_screen.dart
+++ onboarding/lib/screens/onboarding_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       body: MainScreen(),
+    return MaterialApp(
+       home: MainScreen(),
     );
   }
 }
@@ -17,7 +17,49 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: AnnotatedRegion(
+        value: ThemeMode.system,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.1,0.4,0.7,0.9],
+              colors: [
+                Color(0xFF3594DD),
+                Color(0xFF4563DB),
+                Color(0xFF4563DB),
+                Color(0xFF5B16D0),
+              ]
+            ),
+          ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: FlatButton(
+                        onPressed: (){
+                          print("skip");
+                        },
+                        child: Text(
+                          'SKIP',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+        ),
+      ),
+    );
   }
 }
 
