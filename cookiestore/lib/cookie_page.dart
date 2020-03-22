bce75a8c69969cc42d5e4e85040f2239c5b4b3a4
Aximilli1212 <aximill1212@gmail.com>
2020-03-22 13:02:57 +0000

    add buildCard widget for productCard view

--- cookiestore/lib/cookie_page.dart
+++ cookiestore/lib/cookie_page.dart
@@ -18,11 +18,40 @@ class CookiePage  extends StatelessWidget {
               crossAxisSpacing: 10.0,
               mainAxisSpacing: 15.0,
               childAspectRatio: 0.8,
-              children: <Widget>[],
+              children: <Widget>[
+                _buildCard('Cookie mint',   '\$31.44', 'assets/cookiemint.jpg', false, false, context),
+                _buildCard('Cookie cream',   '\$43.44', 'assets/cookiecream.jpg', false, false, context),
+                _buildCard('Cookie classic',   '\$23.44', 'assets/cookieclassic.jpg', false, false, context),
+                _buildCard('Cookie choco',   '\$34.44', 'assets/cookiechoco.jpg', false, false, context),
+              ],
             ),
           )
         ],
       ),
     );
   }
+
+  Widget _buildCard(String name, String price, String imgPath, bool added, bool isFavorite, context){
+     return Padding(
+       padding: EdgeInsets.only(top: 15.0, bottom: 5.0, left: 5.0, right: 5.0),
+       child: InkWell(
+         onTap: (){},
+         child: Container(
+           decoration: BoxDecoration(
+             borderRadius: BorderRadius.circular(15.0),
+             boxShadow: [
+               BoxShadow(
+                 color:  Colors.grey.withOpacity(0.2),
+                 spreadRadius: 3.0,
+                 blurRadius: 5.0,
+               ),
+             ],
+             color: Colors.white,
+           ),
+         ),
+       ),
+     );
+  }
+
 }
+
