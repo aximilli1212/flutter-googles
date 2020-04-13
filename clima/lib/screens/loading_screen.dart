de58fa8d6025bde17e16f3217f76e7b3dfabca4d
Aximilli1212 <aximill1212@gmail.com>
2020-04-13 08:04:19 +0000

    use http package as an object

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,7 +20,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async{
-    Response response = await get(
+    http.Response response = await http.get(
    'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1');
     print(response);
   }
