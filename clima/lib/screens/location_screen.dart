747b9c96760926df6abe49a7aff7e383bb4b8e7e
Aximilli1212 <aximill1212@gmail.com>
2020-04-14 19:05:56 +0000

    add check for a null weatherData before setState and build

--- clima/lib/screens/location_screen.dart
+++ clima/lib/screens/location_screen.dart
@@ -34,6 +34,14 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData){
     setState((){
+
+      if(weatherData == null){
+        temperature = 0;
+        weatherMessage = "Unable to get weather data.";
+        weatherIcon = 'Error';
+        cityName = "Unknown";
+        return;
+      }
     print(weatherData);
     print('we updateing UI');
     double temp = weatherData['main']['temp'];
@@ -82,6 +90,7 @@ class _LocationScreenState extends State<LocationScreen> {
                   FlatButton(
                     onPressed: () async {
                       var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
                     },
                     child: Icon(
                       Icons.location_city,
