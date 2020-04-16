c3e932a8ece57c78a7dee75f2790926aea90396d
Aximilli1212 <aximill1212@gmail.com>
2020-04-16 00:27:06 +0000

    refactor as widget

--- flash_chat/lib/components/rounded_button.dart
+++ flash_chat/lib/components/rounded_button.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 
-Padding FlashRoundedButton({BuildContext context, String bText,Color bColor, Function onTap}) {
-  return Padding(
-    padding: EdgeInsets.symmetric(vertical: 16.0),
-    child: Material(
-      elevation: 5.0,
-      color: bColor,
-      borderRadius: BorderRadius.circular(30.0),
-      child: MaterialButton(
-        onPressed: () {
-          onTap();
-          //Go to login screen.
-        },
-        minWidth: 200.0,
-        height: 42.0,
-        child: Text(
-          bText,
+class FlashRoundedButton extends StatelessWidget {
+  FlashRoundedButton({this.bText,this.bColor,this.onTap});
+
+  final String bText;
+  final Color bColor;
+  final Function onTap;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: bColor,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: () {
+            onTap();
+            //Go to login screen.
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            bText,
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
