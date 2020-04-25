c3f5d531ea21a78d28e901461fb48a9ab34e8ec1
Aximilli1212 <aximill1212@gmail.com>
2020-04-25 15:27:51 +0000

    update state of checkbox and abstract

--- todoey/lib/components/TaskTile.dart
+++ todoey/lib/components/TaskTile.dart
@@ -7,7 +7,27 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(value: false,  onChanged: null),
+      trailing: TaskCheckBox(),
     );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,  onChanged: (newValue){
+          setState((){
+            isChecked = newValue;
+          });
+    });
   }
 }
--- todoey/lib/screens/add_task.dart
+++ todoey/lib/screens/add_task.dart
@@ -18,8 +18,43 @@ class AddTaskScreen extends StatelessWidget {
             borderRadius: BorderRadius.only(topLeft: Radius.circular(40.0), topRight: Radius.circular (40.0))
         ),
         child: Center(
-          child: Text(
-              'I am brave, i am bruised.'
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  'Add Task',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 30.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    autofocus: true,
+                    cursorColor: Colors.lightBlueAccent,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.lightBlueAccent),
+                      fillColor: Colors.lightBlueAccent,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                RaisedButton(
+                  onPressed:(){
+                },
+
+                  child: Text('Add', style: TextStyle(color: Colors.white),),
+                  color: Colors.lightBlueAccent,
+                )
+              ],
+            ),
           ),
         ),
       ),
