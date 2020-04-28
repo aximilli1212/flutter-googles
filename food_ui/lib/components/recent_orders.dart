355f3864913bf744cd881621f15db37731b4077b
Aximilli1212 <aximill1212@gmail.com>
2020-04-28 23:53:27 +0000

    add hori scroll container

--- food_ui/lib/components/recent_orders.dart
+++ food_ui/lib/components/recent_orders.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_ui/data/data.dart';
+import 'package:food_ui/models/order.dart';
 
 class RecentOrders extends StatelessWidget {
   @override
@@ -14,7 +16,19 @@ class RecentOrders extends StatelessWidget {
             letterSpacing: 1.2,
           ),
           ),
-        )
+        ),
+        Container(
+         height: 120.0,
+          color: Colors.grey,
+          child: ListView.builder(
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index){
+              Order order = currentUser.orders[index];
+              return Text(order.food.name);
+
+            },
+          ),
+    )
       ],
     );
   }
