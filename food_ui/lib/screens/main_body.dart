4513853b572f24e140a8d86bb9fe15e4cc20c4e6
Aximilli1212 <aximill1212@gmail.com>
2020-04-28 23:10:45 +0000

    add recent orders view

--- food_ui/lib/screens/main_body.dart
+++ food_ui/lib/screens/main_body.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:food_ui/components/recent_orders.dart';
 
 class MainBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        TextField(
-        decoration: InputDecoration(
-          border:OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30.0),
-              borderSide: BorderSide(width: 0.8,color: Colors.deepOrange)
-
-          )
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+            filled: true,
+            fillColor: Colors.white,
+            border:OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide(width: 0.8,color: Colors.deepOrange),
+            ),
+            enabledBorder:OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            borderSide: BorderSide(width: 0.8,color: Colors.deepOrange),
+    ),
+            hintText: 'Search Food or Restaurant',
+            prefixIcon: Icon(Icons.search, size: 30.0),
+            suffixIcon: IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.close, color: Colors.grey),
+            )
+          ),
+          ),
         ),
-        )
+        RecentOrders(),
       ],
     );
   }
