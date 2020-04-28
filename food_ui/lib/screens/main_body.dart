9ef650f0e4d9318e21df2b0a86038d74b92598a6
Aximilli1212 <aximill1212@gmail.com>
2020-04-28 22:50:51 +0000

    add main body widgets

---
+++ food_ui/lib/screens/main_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        TextField(
+
+        )
+      ],
+    );
+  }
+}
