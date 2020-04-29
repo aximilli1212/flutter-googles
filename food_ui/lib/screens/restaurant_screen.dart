58467f58b533a556b0ff0e013363458323de5d31
Aximilli1212 <aximill1212@gmail.com>
2020-04-29 16:24:30 +0000

    Add stacked icons on restaurant hero

--- food_ui/lib/screens/restaurant_screen.dart
+++ food_ui/lib/screens/restaurant_screen.dart
@@ -24,6 +24,27 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                 width: MediaQuery.of(context).size.width,
                 fit: BoxFit.cover,
                 image: AssetImage(widget.restaurant.imageUrl),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 60.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      color:Colors.white,
+                      iconSize: 30.0,
+                      onPressed: ()=>Navigator.pop(context),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      color:Theme.of(context).primaryColor,
+                      iconSize: 30.0,
+                      onPressed: (){},
+                    ),
+
+                  ],
+    ),
               )
             ],
           )
