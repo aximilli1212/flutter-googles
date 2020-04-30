7d6eadf12dd6d5aa01ee48c5d15bf16b95db5b52
Aximilli1212 <aximill1212@gmail.com>
2020-04-30 18:40:10 +0000

    add translucent overlay

--- food_ui/lib/components/nearby/build_menu_item.dart
+++ food_ui/lib/components/nearby/build_menu_item.dart
@@ -10,20 +10,40 @@ class BuildMenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-       children: <Widget>[
-         Container(
-           height: 175.0,
-           width: 175.0,
-           decoration: BoxDecoration(
-             image: DecorationImage(
-                 image: AssetImage(
-                     food.imageUrl
-                 ),
+    return Center(
+      child: Stack(
+         children: <Widget>[
+           Container(
+             height: 175.0,
+             width: 175.0,
+             decoration: BoxDecoration(
+               image: DecorationImage(
+                   image: AssetImage(
+                       food.imageUrl
+                   ),
+                 fit: BoxFit.cover,
+               ),
+               borderRadius: BorderRadius.circular(15.0)
              ),
            ),
-         )
-       ],
+           Container(
+             height: 175,
+             width: 175,
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(15.0),
+               gradient: LinearGradient(
+                   begin: Alignment.topRight,
+                   end: Alignment.bottomLeft,
+                   colors: [
+                     Colors.black.withOpacity(0.3),
+                     Colors.black87.withOpacity(0.3),
+                     Colors.black54.withOpacity(0.3),
+                     Colors.black38.withOpacity(0.3),
+               ])
+             ),
+           )
+         ],
+      ),
     );
   }
 }
