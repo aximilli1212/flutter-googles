a7e357489ccec4b5dfb7fccb4a65899d3107acd5
Aximilli1212 <aximill1212@gmail.com>
2020-05-05 12:02:31 +0000

    redesign chart card

--- budget_app/lib/widgets/bar_chart.dart
+++ budget_app/lib/widgets/bar_chart.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class BarChart extends StatelessWidget {
+
+  BarChart({this.spending});
+
+  final List<double> spending;
+
   @override
   Widget build(BuildContext context) {
     return Container();
