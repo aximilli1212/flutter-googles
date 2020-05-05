3ab0f1767bef9a7a261799b63134b2fffbbb97a5
Aximilli1212 <aximill1212@gmail.com>
2020-05-05 12:34:16 +0000

    add in all parameters for bar

--- budget_app/lib/widgets/bar.dart
+++ budget_app/lib/widgets/bar.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Bar extends StatelessWidget {
+
+  final String label;
+  final double amountSpent;
+  final double mostExpensive;
+  final double _maxBarHeight = 150.0;
+
+
+  Bar({this.label,this.amountSpent,this.mostExpensive})
+
+
   @override
   Widget build(BuildContext context) {
+    final barHeight = amountSpent / mostExpensive * _maxBarHeight;
+
     return Container();
   }
 }
