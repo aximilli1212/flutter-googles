83d9eaf09a051695332edf283fd086c015019eaf
Aximilli1212 <aximill1212@gmail.com>
2020-05-06 11:33:51 +0000

    add build category wit imports

---
+++ budget_app/lib/widgets/build_category.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:budget_app/models/category_model.dart';
+
+class BuildCategory extends StatelessWidget {
+
+
+  BuildCategory({this.category,this.totalAmountSpent});
+  final Category category;
+  final double totalAmountSpent;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
