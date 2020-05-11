9fb72763060fc1f81d8c47affe39a60e13f24e2f
Aximilli1212 <aximill1212@gmail.com>
2020-05-11 01:02:24 +0000

    add code for color_helper

--- budget_app/lib/helpers/color_helper.dart
+++ budget_app/lib/helpers/color_helper.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+Color getColor(BuildContext context, double percent){
+  if(percent > 0.5){
+    return Theme.of(context).primaryColor;
+  }else if(percent >= 2.5){
+    return Colors.orange}
+}    Colors.redreturn
