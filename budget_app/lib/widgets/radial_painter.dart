9d071684456c92f78d5206f0cc6ef0c13dd43a91
Aximilli1212 <aximill1212@gmail.com>
2020-05-12 06:16:23 +0000

    add in custom overrides for CustomPainter with its own params

--- budget_app/lib/widgets/radial_painter.dart
+++ budget_app/lib/widgets/radial_painter.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 
+class RadialPainter extends CustomPainter{
+
+  final Color bgColor;
+  final Color lineColor;
+  final double percent;
+  final double width;
+
+  RadialPainter({this.bgColor,this.lineColor,this.percent,this.width});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return null;
+  }
+
+}
