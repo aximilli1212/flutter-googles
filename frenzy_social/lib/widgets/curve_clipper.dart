8b7a3f9c4e20081dd114ec388fe62b9cd6341d07
Aximilli1212 <aximill1212@gmail.com>
2020-05-12 22:43:06 +0000

    add in second curve for endPoint

--- frenzy_social/lib/widgets/curve_clipper.dart
+++ frenzy_social/lib/widgets/curve_clipper.dart
@@ -15,6 +15,16 @@ class CurveClipper extends CustomClipper<Path>{
         centerPoint.dy,
     );
 
+    Offset curvePoint2 = Offset(3 * size.width / 4, 3 * size.height / 5);
+    Offset endPoint = Offset(size.width, 4 * size.height / 5);
+
+    path.quadraticBezierTo(
+        curvePoint2.dx,
+        curvePoint2.dy,
+        endPoint.dx,
+        endPoint.dy,
+    );
+
     path.close();
     return path;
   }
