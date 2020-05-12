bd837d5be14ab76ad6654562a2fe69d58a341529
Aximilli1212 <aximill1212@gmail.com>
2020-05-12 05:47:19 +0000

    nav to Category Screen with cat details

--- budget_app/lib/screens/category_screen.dart
+++ budget_app/lib/screens/category_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:budget_app/models/category_model.dart';
 
 class CategoryScreen extends StatefulWidget {
+
+  final Category category;
+  CategoryScreen({this.category});
+
   @override
   _CategoryScreenState createState() => _CategoryScreenState();
 }
@@ -11,7 +15,14 @@ class _CategoryScreenState extends State<CategoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(),
+        title: Text(widget.category.name),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            iconSize: 30.0,
+            onPressed: (){},
+          )
+        ],
       ),
     );
   }
