c018133440b84f286ca42eb7dd75f30b5a6da2c9
Aximilli1212 <aximill1212@gmail.com>
2020-05-12 05:58:50 +0000

    create radial progress box home

--- budget_app/lib/screens/category_screen.dart
+++ budget_app/lib/screens/category_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:budget_app/models/expense_model.dart';
 import 'package:budget_app/models/category_model.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -11,8 +12,14 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+
   @override
   Widget build(BuildContext context) {
+
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense){})
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -23,6 +30,26 @@ class _CategoryScreenState extends State<CategoryScreen> {
             onPressed: (){},
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0,2),
+                  blurRadius: 6.0,
+                )]
+              ),
+            ),
+        ],
+        ),
       ),
     );
   }
