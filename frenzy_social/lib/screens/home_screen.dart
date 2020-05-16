bc9b75132e496f43fe71851245ebf403c2a84e5e
Aximilli1212 <aximill1212@gmail.com>
2020-05-16 21:36:21 +0000

    add base appbar for home page

--- frenzy_social/lib/screens/home_screen.dart
+++ frenzy_social/lib/screens/home_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Center(
+          child: Text('FRENZY', style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),),
+        ),
+      ),
+    );
   }
 }
