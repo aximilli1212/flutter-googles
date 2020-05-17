a022faffae546476861f469027e51cbab0dbea44
Aximilli1212 <aximill1212@gmail.com>
2020-05-17 13:42:23 +0000

    add in main profile image background top

--- frenzy_social/lib/screens/home_screen.dart
+++ frenzy_social/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frenzy_social/data/data.dart';
 import 'package:frenzy_social/models/user_model.dart';
+import 'package:frenzy_social/widgets/custom_drawer.dart';
 import 'package:frenzy_social/widgets/following_users.dart';
 import 'package:frenzy_social/widgets/posts_carousel.dart';
 
